b7147da20934ad4c3e8c703246aef2a32b67d85a
Filipe Borges <filipborgs48@gmail.com>
2023-01-25 11:08:00 -0300

    suport for discorver kde store

--- install/programs.sh
+++ install/programs.sh
@@ -2,6 +2,6 @@
 yay -S wps-office ttf-wps-fonts microsoft-edge-beta-bin
 
 ##kde
-sudo pacman -S ark partitionmanager  gwenview  kdeconnect qbittorrent vlc kolourpaint --noconfirm
+sudo pacman -S ark partitionmanager  gwenview  kdeconnect qbittorrent vlc kolourpaint packagekit-qt5 --noconfirm
 
 sudo pacman -S firefox  neofetch gnome-keyring --noconfirm
